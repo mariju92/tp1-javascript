47e4bb6cc0b47fdc8d3ff2f472524ed0bffe10b5
Marianjuavi <mariana.ju92@gmail.com>
2023-03-01 02:38:09 -0300

    Se cambio que el resultado se mostrará en consola para que se muestre en pantalla

--- js/ejercicio4.js
+++ js/ejercicio4.js
@@ -1,3 +1,3 @@
 // 4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
 let nombreUsuario = prompt("¿Cuál es tu nombre?");
-console.log("Hola " + nombreUsuario);
+document.write("Hola " + nombreUsuario);
